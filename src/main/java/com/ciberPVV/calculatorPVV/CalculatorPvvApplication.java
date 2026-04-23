e48d4bd2d9ec250ec3f56ce5345b330cff68dce1
Pelayo Vázquez <pelayovv20@educastur.es>
2026-04-23 20:28:51 +0200

    Añadido esqueleto Spring Boot

---
+++ src/main/java/com/ciberPVV/calculatorPVV/CalculatorPvvApplication.java
@@ -0,0 +1,13 @@
+package com.ciberPVV.calculatorPVV;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalculatorPvvApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CalculatorPvvApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ciberPVV/calculatorPVV/CalculatorPvvApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ciberPVV.calculatorPVV;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalculatorPvvApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
